de759041d163657e6a9eb935400e6bee1ba41fb5
thdwnsdlf61 <il4500@naver.com>
2025-01-09 00:18:42 +0900

    refactor : 코드 일부 수정3

--- src/main/java/com/outsourcing/controller/store/StoreController.java
+++ src/main/java/com/outsourcing/controller/store/StoreController.java
@@ -5,7 +5,6 @@ import com.outsourcing.service.store.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
