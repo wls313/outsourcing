c757860421ddd48b00034b657278f25b52166f0f
KIMHAKSAN <bk351313@naver.com>
2025-01-08 12:51:26 +0900

    feat: OrderStatus 작성

---
+++ src/main/java/com/outsourcing/entity/order/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.outsourcing.entity.order;
+
+public enum OrderStatus {
+    ORDER_CONFIRMED,    // 주문 확인중
+    COOKING_STARTED,    // 조리 시작
+    DELIVERY_IN_PROGRESS,  // 배달중
+    DELIVERY_COMPLETED  // 배달 완료
+}
