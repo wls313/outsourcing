d099689b8fab7addbd5653f948fbc6e3bd0efc83
JinsolSong <jeanies@naver.com>
2025-01-10 16:51:37 +0900

    feat : 오더 파트 생성 오더 API생성

--- src/main/java/com/outsourcing/dto/order/OrderRequestDto.java
+++ src/main/java/com/outsourcing/dto/order/OrderRequestDto.java
@@ -1,11 +1,6 @@
 package com.outsourcing.dto.order;
 
-import com.outsourcing.common.entity.order.Order;
-import com.outsourcing.common.entity.order.OrderStatus;
-
-import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,30 +9,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor // 모든 필드를 초기화하는 생성자
 public class OrderRequestDto {
 
+    //유저 아이디
+    //스토어 아이디
+    //메뉴 아이디
 
-    @NotBlank(message = "User name is required")
-    private String userName;
+    private Long menuId;
 
-    @NotBlank(message = "Store name is required")
-    private String storeName;
+    private Long storeId;
 
-    @Positive(message = "Menu price must be greater than 0")
-    private int menuPrice;
+    private Long userId;
 
-    @NotBlank(message = "Menu name is required")
-    private String menuName;
 
-    @Pattern(regexp = "ORDER_CONFIRMED|COOKING_STARTED|DELIVERY_IN_PROGRESS|DELIVERY_COMPLETED",
-            message = "Invalid order status")
-    private String status;
 
-    public Order toOrder() {
-        return new Order(
-                userName,
-                storeName,
-                menuPrice,
-                menuName,
-                OrderStatus.valueOf(status.toUpperCase())
-        );
-    }
 }
--- src/main/java/com/outsourcing/repository/order/OrderRepository.java
+++ src/main/java/com/outsourcing/repository/order/OrderRepository.java
@@ -2,6 +2,19 @@ package com.outsourcing.repository.order;
 
 import com.outsourcing.common.entity.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT o FROM Order o WHERE o.id = :id")
+    Optional<Order> findByOrderId(@Param("id") Long id);
+
+    List<Order> findByUserId(Long userId);
+
+    List<Order> findByStoreId(Long storeId);
+
 }
