8caabe8048cc8327db2c6a6b9a447940463e3875
thdwnsdlf61 <il4500@naver.com>
2025-01-12 03:35:23 +0900

    refactor: 예외처리 강화
    메뉴 단건조회
    -> 가게에서 메뉴를 리스트로 출력하도록 로직 생성
    -> 폐업한 가게를 조회할때 예외처리
    -> 서비스 로직에서 발생하는 예외를 GlobalExceptionHandler에서 관리하도록 수정

--- src/main/java/com/outsourcing/repository/menu/MenuRepository.java
+++ src/main/java/com/outsourcing/repository/menu/MenuRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu,Long> {
 
-    Menu findByStoreId(Long findStoreById);
+//    Menu findByStoreId(Long findStoreById);
 
     @Query("select m from Menu m where m.store.id = :storeId")
     List<Menu> findMenuByStoreId(Long storeId);
